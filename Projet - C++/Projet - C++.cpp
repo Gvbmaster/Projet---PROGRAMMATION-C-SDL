d519a2286ce12ecf1ebe04b2d0c97a9f8825fb92
gnangndong <gnangndong@gaming.tech>
2023-10-25 13:38:52 +0200

    essai

--- Projet - C++/Projet - C++.cpp
+++ Projet - C++/Projet - C++.cpp
@@ -1,6 +1,33 @@
 #include <vector>
 #include <iostream>
 
+struct Player
+{
+	Point position;
+};
+
+class Point 
+{
+public:
+	int x;
+	int y;
+	int z;
+
+	void Clear() 
+	{
+		x = 0;
+		y = 0;
+		z = 0;
+	}
+};
+
 int main() {
+
+	int a;
+	float b;
+
+
+	Player player;
+	player.position.Clear();
 
 };

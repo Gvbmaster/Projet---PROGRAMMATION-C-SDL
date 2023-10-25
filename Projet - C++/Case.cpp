556050457885fdc11a980e1a9b135ed6f6e2d55b
gnangndong <gnangndong@gaming.tech>
2023-10-25 13:16:56 +0200

    Création de la classes case.

---
+++ Projet - C++/Case.cpp
@@ -0,0 +1,29 @@
+#include "Case.h"
+
+class Case {
+public:
+    Case();
+    int getValeur() const;
+    void setValeur(int nouvelleValeur);
+    bool estVide() const;
+
+private:
+    int valeur; // Valeur de la case (0 pour une case vide, sinon une puissance de 2)
+};
+
+Case::Case() : valeur(0) {
+}
+
+int Case::getValeur() const {
+    return valeur;
+}
+
+void Case::setValeur(int nouvelleValeur) {
+    valeur = nouvelleValeur;
+}
+
+bool Case::estVide() const {
+    return (valeur == 0);
+}
+
+
--- Projet - C++/Projet - C++.cpp
+++ Projet - C++/Projet - C++.cpp
@@ -1,30 +1,6 @@
 #include <vector>
 #include <iostream>
 
+int main() {
 
-class Case {
-public:
-    Case();
-    int getValeur() const;
-    void setValeur(int nouvelleValeur);
-    bool estVide() const;
-
-private:
-    int valeur; // Valeur de la case (0 pour une case vide, sinon une puissance de 2)
 };
-
-Case::Case() : valeur(0) {
-}
-
-int Case::getValeur() const {
-    return valeur;
-}
-
-void Case::setValeur(int nouvelleValeur) {
-    valeur = nouvelleValeur;
-}
-
-bool Case::estVide() const {
-    return (valeur == 0);
-}
-
